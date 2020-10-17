689b4f5c4bd68ee4b64eaaeeb6901496fc3677d6
Diego Ribeiro <dmrbeiro87@gmail.com>
2020-10-17 00:05:40 -0300

    Create entity Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,26 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+
         Department department = new Department(1, "Books");
 
+        Seller seller = new Seller(
+                1,
+                "Diego Ribeiro",
+                "dmribeiro@gmail.com",
+                new Date(),
+                2100.00,
+                department);
+
         System.out.println(department);
+        System.out.println(seller);
     }
 }
