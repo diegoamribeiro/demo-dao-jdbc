ca34ed6b0b08aca3369934d6be88745ddbf5a818
Diego Ribeiro <dmrbeiro87@gmail.com>
2020-10-17 19:31:42 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,19 +11,11 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) throws ParseException {
 
-        Department department = new Department(1, "Books");
-
-        Seller seller = new Seller(
-                1,
-                "Diego Ribeiro",
-                "dmribeiro@gmail.com",
-                new Date(),
-                2100.00,
-                department);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
 
-        System.out.println(department);
         System.out.println(seller);
+
+
     }
 }
